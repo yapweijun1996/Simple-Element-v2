a237aeb47360a9fb4ab039afd39637adf298ede5
YAP WEI JUN <yapweijun1996@gmail.com>
2025-05-02 20:46:53 +0800

    zz

--- src/components/CardGrid.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: 'CardGrid',
-  template: `
-    <div class="cards">
-      <slot></slot>
-    </div>
-  `
-};
--- src/views/ReportsView.js
+++
@@ -1,9 +0,0 @@
-export default {
-  name: 'ReportsView',
-  template: `
-    <div>
-      <h2>Reports</h2>
-      <p>Reports coming soon...</p>
-    </div>
-  `
-};
